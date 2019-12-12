ae2ad44ded39cc4176cb9ad77e7bd8627af5161e
raq213 <46656365+raq213@users.noreply.github.com>
2019-12-12 18:43:32 +0100

    Added changes, database description, comments

--- logisticRegression.py
+++ logisticRegression.py
@@ -4,16 +4,26 @@ from sklearn.linear_model import LogisticRegression
 
 #dataset of the candidates
 #in admission 0 is the false value (not admitted) and 1 true value (admitted)
+
+#DATABASE COMPONENTS MEANING)
+#gmat- Graduate Management Admission Test (result of a test)
+#gpa-Grade Point Average (average of grades)
+#work experience (in years)
+#admission to the university (1-YES, 0-NO)
+
+#database components are copied directly to the script 
+
 candidates = {'gmat': [780,750,690,710,680,730,690,720,740,690,610,690,710,680,770,610,580,650,540,590,620,600,550,550,570,670,660,580,650,660,640,620,660,660,680,650,670,580,590,690],
               'gpa': [4,3.9,3.3,3.7,3.9,3.7,2.3,3.3,3.3,1.7,2.7,3.7,3.7,3.3,3.3,3,2.7,3.7,2.7,2.3,3.3,2,2.3,2.7,3,3.3,3.7,2.3,3.7,3.3,3,2.7,4,3.3,3.3,2.3,2.7,3.3,1.7,3.7],
               'workExperience': [3,4,3,5,4,6,1,4,5,1,3,5,6,4,3,1,4,6,2,3,2,1,4,1,2,6,4,2,6,5,1,2,4,6,5,1,2,1,4,5],
               'admission': [1,1,1,1,1,1,0,1,1,0,0,1,1,1,1,0,0,1,0,0,0,0,0,0,0,1,1,0,1,1,0,0,1,1,1,0,0,0,0,1]
               }
-
+#applying pandas Dataframe 
 data = pandas.DataFrame(candidates,columns= ['gmat', 'gpa','workExperience','admission'])
 
-X = data[['gmat', 'gpa','workExperience']]
-y = data['admission']  
+#setting the independend and dependend variables 
+X = data[['gmat', 'gpa','workExperience']] #independent
+y = data['admission']  #dependent
 
 #75% is the train mode and rest 25% are test results
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.25,random_state=0)  
@@ -34,6 +44,7 @@ new_candidates = {'gmat': [590,740,680,610,710],
 
 newData = pandas.DataFrame(new_candidates,columns= ['gmat', 'gpa','workExperience'])
 y_pred=logistic_regression.predict(newData)
+
 
 #test dataset (without the actual outcome)
 #print (X_test)
